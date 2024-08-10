3a3af675448f37f8573368fdd9c2d1e80f21e81e
martinjauma <martin.jauma@gmail.com>
2024-08-10 00:03:35 -0300

    FUNCIONANDO

--- src/reports/dashboard_Data_Base.py
+++ src/reports/dashboard_Data_Base.py
@@ -1,14 +1,7 @@
 import streamlit as st
 
 def main():
-    st.title("Teros")
-
-    # Agregar video usando st.video() con estilo CSS para ajustar tamaño
-    
-    col1, col2, col3, col4,col5 = st.columns([1,1,2,1,1])
-    
-    with col3:
-        st.video("app/assets/Teos.mp4")
+    st.title("Dashboard Base de Datos")
    
 if __name__ == "__main__":
     main()
